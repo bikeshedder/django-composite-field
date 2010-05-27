bc099b04174f303ff932cdd37873c5b93b1a3bde
Michael P. Jung <mpjung@terreon.de>
2010-05-27 17:34:07 +0200

    Add keywords to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='django-composite-field',
       version='0.1',
       description='CompositeField implementation for Django',
+      keywords='django composite field',
       author='Michael P. Jung',
       author_email='mpjung@terreon.de',
       url='http://bitbucket.org/mp/django-composite-field',
