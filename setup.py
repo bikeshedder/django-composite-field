d88bb4aafbfbf82ce027e72bf71156e5537adf42
Michael P. Jung <michael.jung@terreon.de>
2015-07-23 19:14:43 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup(name='django-composite-field',
-      version='0.2',
-      description='CompositeField implementation for Django',
-      keywords='django composite field',
-      author='Michael P. Jung',
-      author_email='michael.jung@terreon.de',
-      url='http://bitbucket.org/bikeshedder/django-composite-field',
-      packages=['composite_field'],
-     )
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# Prevent "TypeError: 'NoneType' object is not callable" when running tests.
+# (http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+
+setup(
+    name='django-composite-field',
+    version='0.2',
+    description='CompositeField implementation for Django',
+    long_description=read('README'),
+    author='Michael P. Jung',
+    author_email='michael.jung@terreon.de',
+    license='BSD',
+    keywords='django composite field',
+    url='http://bitbucket.org/bikeshedder/django-composite-field',
+    packages=['composite_field'],
+    test_suite='composite_field.tests',
+    tests_require=['Django'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
