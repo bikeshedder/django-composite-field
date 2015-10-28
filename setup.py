8c2461010adffe0cfb1be3e7161c3f0d7002d444
Michael P. Jung <michael.jung@terreon.de>
2015-10-28 14:32:41 +0100

    Version 0.7.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class DjangoTestCommand(Command):
 
 setup(
     name='django-composite-field',
-    version='0.7.0',
+    version='0.7.1',
     description='CompositeField implementation for Django',
     long_description=read('README'),
     author='Michael P. Jung',
