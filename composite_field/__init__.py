16ed424e1f284b4a25f57f23f9855fa61788e6ca
Michael P. Jung <michael.jung@terreon.de>
2020-01-06 18:50:48 +0100

    Version 1.1.0

--- composite_field/__init__.py
+++ composite_field/__init__.py
@@ -2,4 +2,4 @@ from .base import CompositeField
 from .l10n import LocalizedField, LocalizedCharField, LocalizedTextField
 from .complex import ComplexField
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
