d6a9dc020b6f3a94b4760969f7453f70da2b07b2
Michael P. Jung <michael.jung@terreon.de>
2024-02-01 18:41:38 +0100

    Fix usage of RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,11 +47,7 @@ master_doc = 'index'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if not on_rtd:
-    # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
